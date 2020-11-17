3c8780593daac70ae9201cbc07b884d56f0b9db9
sagbeyeg <agbeyegbesisan@gmail.com>
2020-11-17 14:38:14 -0500

    basic deliverable done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import './App.css';
-function App() {
-  return <h1>You Can Do This!</h1>;
+import { yes, no } from "./objects";
+class App extends React.Component {
+  state = {
+    clicked: false
+  }
+
+  clickHandler = () => {
+    this.setState((prevState) => ({clicked: !prevState.clicked}))
+  }
+
+
+  render() {
+    return (
+    <div>
+      <span className="header">
+        <h1>You Can Do This!</h1>
+      </span>
+      <span className="response">
+        <img onClick={this.clickHandler} src={this.state.clicked ? yes["yes-image"] : no["no-image"]} alt={this.state.clicked ? 'yes-image' : 'no-image'} />
+        {this.state.clicked ? (<h1>{yes["yes-statement"]}</h1>) : (<h1>{no["no-statement"]}</h1>)}
+      </span>
+    </div>
+    )
+  } 
 }
 
 export default App;
